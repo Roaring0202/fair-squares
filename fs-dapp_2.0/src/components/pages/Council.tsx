4731e836df25f6d0adeef8e326227694e4613365
edwardpower116 <oliveranderson116ok@outlook.com>
2024-03-31 00:07:00 +0900

    Fixed all Council refresh bugs

--- fs-dapp_2.0/src/components/pages/Council.tsx
+++ fs-dapp_2.0/src/components/pages/Council.tsx
@@ -19,75 +19,62 @@ export default function Council() {
   const { api, blocks, selectedAccount,accounts,  dispatch } = useAppContext();
   const { role, balance, dispatch0 } = useAccountContext();
   const { session_closed,approved,role_in_session,nay,ayes,council_members,selectedProposal,proposals, datas,dispatch1 } = useConcilSessionContext();
+  const CODE_KEY = "0x98d32a3a7be412e9bda0bf02ccbdec683a1e528d74cc03d161a7a2fd1d02d98a"
   
  
-  const [hash0,setHash0] = useState<string[]>([]);
+  const [ln,setLn] = useState(0); 
 
-  function querryProposals(hash:string) {
+  function getDatas(){
+    let tdata:DataType[]=[]
+    
+    api?.query.rolesModule.requestedRoles.entries((all:any[])=>{
+      
 
-    api?.query.backgroundCouncil.proposalOf(
-      
-      hash,(data_acc:any)=>{
-        if(!data_acc) return;
-        console.log(`datas:${data_acc}`);
-        let acc = data_acc.toPrimitive().args.account as InjectedAccountWithMeta;
-      let acc0 = data_acc.toPrimitive().args.account as string;
-      const acc1 = accounts.find((account) => account.address === acc0);
-      
+       all.forEach(([key, value]) => {
+        let Prop = value.toHuman();
+        if(!Prop) return;
+        let acc0 = Prop.accountId;
+        let acc1 = accounts.find((account) => account.address === acc0);
+        if(!acc1) return;
         
-      let acc_list = proposals;
-      if (acc && !acc_list.includes(acc1 as InjectedAccountWithMeta) && acc1){
-        acc_list.push(acc1);
-                             
-        dispatch1({type:`SET_PROPOSALS`,payload:acc_list}); 
-        
-        
-        
-        api.query.rolesModule.requestedRoles(acc1.address,(Prop_raw:any)=>{
-          let Prop = Prop_raw.toHuman();
-      if(!Prop) return;
-      let r_session = Prop.role.toString();
+        console.log(`names are:${acc1.meta.name}`)
+        let r_session = Prop.role.toString();
       let status = Prop.approved.toString();
       let referendum = Prop.sessionClosed.toString();
       let hash = Prop.proposalHash.toString();
-      let dtype:DataType={name:acc1.meta.name,role:r_session,address:acc.address,status,referendum,hash};
-      let tdata=datas
-      
-      tdata.push(dtype);
-      dispatch1({type:`SET_DATAS`,payload:tdata}); 
-        })       
-       
-      }        
-    }
-    );
+      let dtype:DataType={name:acc1.meta.name,role:r_session,address: Prop.accountId,status,referendum,hash};      
+      tdata.push(dtype);      
+              }
+);
+
+dispatch1({type:`SET_DATAS`,payload:tdata});
+    });
+    
   }
 
-
-  const update = useCallback(() =>{    
-    let bb =hash0.map((x)=>querryProposals(x));
-    return bb},[hash0,blocks]);
+  const update = useCallback(() =>{  
+    
+    console.log(`Datas length before:${datas.length}`)
+    let l= datas.length 
+    getDatas();
+    if(l!==datas.length){
+      setLn(datas.length)
+      
+    }
+    
+  },[ln]);
 
 
 
 
    useEffect(() => {
     if (!api || !selectedAccount) return;
-
-    api.query.backgroundCouncil.proposals((hash: string[]) => {
-      if (hash.length > 0) {
-        
-        setHash0(hash);
-        dispatch1({type:`SET_PROPOSALS`,payload:[]});
-        dispatch1({type:`SET_DATAS`,payload:[]}); 
-
-        update() 
-        
-        
-      }    
-    });   
-   
-
-  }, [hash0,selectedAccount,api]);
+    
+    getDatas();
+    
+    
+    console.log(`Datas length after:${datas.length}`)
+  }, [ln,selectedAccount]);
 
  
 
