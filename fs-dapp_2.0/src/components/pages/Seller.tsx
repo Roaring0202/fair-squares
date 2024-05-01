f315608958023eb42a1dbd8137a817ad4aaac0fc
edwardpower116 <oliveranderson116ok@outlook.com>
2024-05-01 17:57:21 +0900

    Get submitted asset and seller infos

--- fs-dapp_2.0/src/components/pages/Seller.tsx
+++ fs-dapp_2.0/src/components/pages/Seller.tsx
@@ -2,29 +2,50 @@ import { useAssetContext } from "../../contexts/AssetContext"
 import { useEffect } from "react";
 import { useConcilSessionContext } from "@/src/contexts/CouncilSessionContext"
 import { useAppContext } from "../../contexts/AppContext"
+import { useAccountContext } from "@/src/contexts/Account_Context";
 import {BN,formatBalance} from '@polkadot/util';
 import { Card} from 'antd';
 import { List } from "antd";
 import { Button  } from 'antd';
-import { SellerData, AssetData,DataType } from "../../contexts/types";
+import { SellerData, AssetData,arrangeText0 } from "../../contexts/types";
 const status0 =["EDITING","REVIEWING","VOTING"]
 
 export default function Seller(){
 
     const {seller,assets,selected_asset,dispatch3} = useAssetContext();
     const { api, blocks, selectedAccount,accounts} = useAppContext();
-
+    const { balance,infos,dispatch0 } = useAccountContext();
     const initAssetData:AssetData = {collection_id:0,item_id:0,owner_address:"",infos:""}
     const initSellerData:SellerData = {name:"",address:"",balance:""}
     
 
     function getAssetDatas(){
         if (!api||!selectedAccount) return;
-        let assets:AssetData[]=[];
-        let tdata:DataType[]=[]
+        let assets0:AssetData[]=[];
+        let seller0 = initSellerData;
+        
     let who = selectedAccount.address;
+    api.query.rolesModule.HouseSellerLog(who,(data:any)=>{
+        if(!data) return;
+        let data0 = data.toHuman();
+        console.log(data0);
+        let txt = arrangeText0(data0.infos,true);
+        seller0.name = txt[0];
+        seller0.address = selectedAccount.address;
+
+        api.query.system.account(who, ({ data: free }: { data: { free: BN } }) => {
+            formatBalance.setDefaults({ decimals: 11, unit: 'USD' });
+            const free0 = formatBalance(free.free,{ withSi: true, withZero: false });
+            
+            dispatch0({ type: 'SET_BALANCE', payload: free0 });
+            seller0.balance = free0;
+            dispatch3({ type:'SET_SELLER' , payload:seller0});
+          });
+
+    });
 
     api.query.nfts.account.keys((all:any[])=>{
+
         all.forEach((key)=>{
             if (!key) return;
             let asset_data=initAssetData;         
@@ -38,21 +59,22 @@ export default function Seller(){
                     let infos0=asset_infos.toHuman();
                     let infos = `${infos0.infos.metadata}:${infos0.price}:${infos0.status}:${infos0.maxTenants}`;
                     asset_data.infos=infos;
+                    assets0.push(asset_data);
                     console.log(asset_data.infos)
                 })
-
             }
-
-            
         }
     )
-    });
+    dispatch3({ type: 'SET_ASSETS', payload:assets0});
 
+    });
     }
+
+   
 
     useEffect(()=>{
         getAssetDatas()
-    },[api,selectedAccount])
+    },[api,selectedAccount,blocks])
     
 
     return(
--- fs-dapp_2.0/src/components/shared/InfosFormAsset.tsx
+++ fs-dapp_2.0/src/components/shared/InfosFormAsset.tsx
@@ -1,13 +1,17 @@
 import {useForm,FieldError} from 'react-hook-form';
 import { useAccountContext } from '../../contexts/Account_Context';
+import { ASSETS } from "../../contexts/types";
 import { Button } from 'flowbite-react';
 
 type Details = {
     picture: string;
-    fullname: string;
-    email: string;
-    asset_website: string;
+    asset_type: string;
+    asset_name: string;
+    asset_price:number;
+    tenants_capacity:number;
     description: string;
+    contact_email: string;
+    website: string;
     notes: string;
 }
 
@@ -25,9 +29,17 @@ export function DetailsPage(){
             pic=`../../../johndoe.png`
            
         }else{pic=details.picture}
-        let all_infos:string = `${details.fullname}:${pic}:${details.email}:${details.asset_website}:${details.description}:${details.notes}`;
+        let all_infos:string = 
+        `${pic}:${details.asset_type}        
+        :${details.asset_name}
+        :${details.asset_price}
+        :${details.tenants_capacity}
+        :${details.description}
+        :${details.contact_email}
+        :${details.website}
+        :${details.notes}`;
+        
         dispatch0({type:`SET_INFOS`,payload:all_infos});
-
 
     }
 
@@ -40,40 +52,75 @@ export function DetailsPage(){
         <div>
             <form noValidate onSubmit={handleSubmit(onSubmit)}>
                 <div className={fieldstyle}>
-                    <label htmlFor='picture'>Link to Avatar Picture</label>
+                    <label htmlFor='picture'>Asset Picture</label>
 
                     <select 
                     id="picture" 
                     {...register('picture')}>
-                    <option value= "../../../APPARTMENTS.png">APPARTMENTS</option>
                     <option value= "../../../HOUSES.png">HOUSES</option>
                     <option value= "../../../OFFICES.png">OFFICES</option>
+                    <option value= "../../../APPARTMENTS.png">APPARTMENTS</option>
                     </select>
                 </div>
+
                 <div className={fieldstyle}>
-                    <label htmlFor='fullname' >Full Name</label>
-                    <input type='text' id="fullname" className={getEditorStyle(errors.fullname)}{...register('fullname',{
-                        required:`You must enter your full name`
+                    <label htmlFor='type'>Asset Type</label>
+
+                    <select 
+                    id="type" 
+                    {...register('asset_type')}>
+                    <option value= {ASSETS[0]}>HOUSES</option>
+                    <option value= {ASSETS[1]}>OFFICES</option>
+                    <option value= {ASSETS[2]}>APPARTMENTS</option>
+                    </select>
+                </div>
+
+                <div className={fieldstyle}>
+                    <label htmlFor='name' >Full Name</label>
+                    <input type='text' id="name" className={getEditorStyle(errors.asset_name)}{...register('asset_name',{
+                        required:`You must enter a name for the asset`
                     })}/>
                 </div>
+
                 <div className={fieldstyle}>
-                    <label htmlFor='email'>e-mail address</label>
-                    <input type='email' id ="email" className={getEditorStyle(errors.email)}{...register('email',{
-                        required:`You must enter your e-mail address`
+                    <label htmlFor='price' >Full Name</label>
+                    <input type='number' id="price" className={getEditorStyle(errors.asset_price)}{...register('asset_price',{
+                        required:`You must enter a price for the asset`
                     })}/>
                 </div>
+
                 <div className={fieldstyle}>
-                    <label htmlFor='asset_website'>One of your social website(Linkedin, Facebook, etc...)</label>
-                    <input type='text' id="asset_website" {...register('asset_website')}/>
+                    <label htmlFor='capacity' >Full Name</label>
+                    <input type='number' id="capacity" className={getEditorStyle(errors.asset_price)}{...register('tenants_capacity',{
+                        required:`You must give a number for maximum possible tenants for the asset.`
+                    })}/>
                 </div>
+
                 <div className={fieldstyle}>
                     <label htmlFor='description'>Subscription Motivation</label>
                     <input type='text' id="description" {...register('description')}/>
                 </div>
+
+
+                <div className={fieldstyle}>
+                    <label htmlFor='email'>e-mail address</label>
+                    <input type='email' id ="email" className={getEditorStyle(errors.contact_email)}{...register('contact_email',{
+                        required:`You must enter your e-mail address`
+                    })}/>
+                </div>
+
+                <div className={fieldstyle}>
+                    <label htmlFor='asset_website'>One of your social website(Linkedin, Facebook, etc...)</label>
+                    <input type='text' id="asset_website"className={getEditorStyle(errors.website)}{...register('website',{
+                        required:`You must enter a website for the asset`
+                    })}/>
+                </div>                
+
                 <div className={fieldstyle}>
                     <label htmlFor='notes'>Additional Informations</label>
                     <input type='text' id="notes" {...register('notes')}/>
                 </div>
+
                 <div>
                     <Button 
                     type="submit"
